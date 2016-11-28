cb4deca53b18a8a10c1877c8346d7ff10013c1da
anna <annjellyiu5@gmail.com>
2016-11-28 12:44:33 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
-
-
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, Link, browserHistory  } from 'react-router'
 import MainComponent from './main'
-//import MainComponent from './bootstrapCart'
+const App = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>Выберите тип пользователя:</h1>
+                <ul>
+                    <li><Link to="/admin">Администроатр</Link></li>
+                    <li><Link to="/user">Пользователь</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
+})
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <MainComponent/>
-        </div>
-    );
-  }
+const About = React.createClass({
+    render() {
+        return <h3>About</h3>
+    }
+})
+
+class Greeting extends React.Component {
+    render() {
+        return  <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <Route path="admin" component={About} />
+                <Route path="user" component={MainComponent}/>
+            </Route>
+        </Router>
+    }
 }
-
-export default App;
+export default Greeting
--- src/main.js
+++ src/main.js
@@ -192,4 +192,4 @@ const MainComponent = React.createClass({
     }
 })
 
-export default MainComponent;
+export default MainComponent
